ad22d29e82b8323e3d01be3fbbe6b93840002693
RyanMinneo <minneor@go.stockton.edu>
2023-02-27 14:40:04 -0500

    Made a checkPalindrome Method utilizing Stack
    Method essentially reverses a string using a stack and then compares it
    to the original. If the original == the reverse, the string is a
    palindrome and the method will return true.

--- MiscProgram/src/DataStructure.java
+++ MiscProgram/src/DataStructure.java
@@ -1,8 +1,38 @@
-/**
- * 
- * @author Ryan Minneo
- *
- */
-public class DataStructure {
+import java.util.Stack;
 
+/**
+ * This program is essentially just testing out different Data Structures and seeing how they will work
+ * @author Ryan Minneo
+ * @version 1.0 - Added First Method checkPalindrome
+ */
+public class DataStructure 
+{
+	//Working with Stacks
+		/**
+		 * This method utilizes a stack to reverse a string and then compares the reverse of the string to the string itself. If the original and reverse of the string are the same, then the string is a palindrome and the method will return true. Otherwise the method will return false.
+		 * 
+		 * @param str - the input string
+		 * @return true/false - true if the string is a palindrome, false otherwise
+		 */
+		public boolean checkPalindrome(String str)
+		{
+			Stack<String> stack = new Stack<String>();
+			String palindrome = "";
+			
+			for (int i = 0; i < str.length(); i++)
+			{
+				stack.push(str.substring(i, i+1));
+			}
+			
+			for (int i = 0; i < str.length(); i++)
+			{
+				palindrome += stack.pop();
+			}
+			
+			if (palindrome.equals(str)) 
+			{
+				return true;
+			}
+			else return false;
+		}
 }
--- MiscProgram/src/DataStructureTester.java
+++ MiscProgram/src/DataStructureTester.java
@@ -1,8 +1,15 @@
 /**
- * 
+ * The tester for all of my DataStructure classes, objects, methods, etc.
  * @author Ryan Minneo
- *
+ * @version 1.0
  */
-public class DataStructureTester {
-
+public class DataStructureTester 
+{
+	public static void main(String[] args)
+	{
+		DataStructure tester = new DataStructure();
+		
+		System.out.println(tester.checkPalindrome("saippuakivikauppias"));
+		System.out.println(tester.checkPalindrome("saippuakivikauppia"));
+	}
 }
